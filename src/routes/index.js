b2ff9dcc694d59ad692296abcc1899a181d4231d
Quocdat210201 <pqd2102@gmail.com>
2022-08-26 09:50:36 +0700

    router layoit

---
+++ src/routes/index.js
@@ -0,0 +1,18 @@
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
+import { HeaderOnly } from '~/components/Layout';
+
+// Public Routes
+const publciRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+];
+
+// Pivate Routes
+const privateRoutes = [];
+
+export { publciRoutes, privateRoutes };
